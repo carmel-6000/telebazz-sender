b4081fca44ab47d01bd093a986de05cd70a1e0e2
shirarab <broomonster@gmail.com>
2018-12-30 17:03:23 +0200

    delete commets in msgCtrlButtons.js

--- telebazz-sender-src/src/scenes/home_page/MsgCtrlButtons.js
+++ telebazz-sender-src/src/scenes/home_page/MsgCtrlButtons.js
@@ -42,14 +42,34 @@ export class MsgCtrlButtons extends Component {
                     ?
                     <div className="dropdown-message-menu">
                         <Link to={`/EditMessage/${this.props.itemID}`}>
-                            <button onClick={() => this.props.editmessage(this.props.itemID)} className="dropdown-item btn btn-primary dropdown-msg-control-item" >ערוך/י</button>
+                            <button
+                                onClick={() =>
+                                    this.props.editmessage(this.props.itemID)}
+                                className="dropdown-item btn btn-primary dropdown-msg-control-item" >
+                                ערוך/י
+                            </button>
                         </Link>
 
-                        <button onClick={(e) => this.props.deletemessage(e, this.props.itemID, this.props.isFav)} className="dropdown-item btn btn-primary dropdown-msg-control-item" >מחק/י</button>
+                        <button
+                            onClick={(e) =>
+                                this.props.deletemessage(e, this.props.itemID, this.props.isFav)}
+                            className="dropdown-item btn btn-primary dropdown-msg-control-item" >
+                            מחק/י
+                        </button>
                         {this.props.isFav ?
-                            <button onClick={(e) => this.props.changelocation(e, this.props.itemID, this.props.isFav)} className="dropdown-item btn btn-primary dropdown-msg-control-item" >הסר/י ממועדפים</button>
+                            <button
+                                onClick={(e) =>
+                                    this.props.changelocation(e, this.props.itemID, this.props.isFav)}
+                                className="dropdown-item btn btn-primary dropdown-msg-control-item" >
+                                הסר/י ממועדפים
+                            </button>
                             :
-                            <button onClick={(e) => this.props.changelocation(e, this.props.itemID, this.props.isFav)} className="dropdown-item btn btn-primary dropdown-msg-control-item" >הוסף/י למועדפים</button>
+                            <button
+                                onClick={(e) =>
+                                    this.props.changelocation(e, this.props.itemID, this.props.isFav)}
+                                className="dropdown-item btn btn-primary dropdown-msg-control-item" >
+                                הוסף/י למועדפים
+                            </button>
                         }
                     </div>
                     : null
@@ -58,32 +78,3 @@ export class MsgCtrlButtons extends Component {
         );
     }
 }
-    // render() {
-    //     return (
-    //         <div>
-    //             <div className="dropdown-ctrl-message">
-    //                 <button
-    //                     className="btn btn-secondary dropdown-toggle"
-    //                     type="button"
-    //                     id="dropdownMenuButton"
-    //                     data-toggle="dropdown"
-    //                     aria-haspopup="true"
-    //                     aria-expanded="false"
-    //                 />
-
-    //                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    //                     <Link to={`/EditMessage/${this.props.itemID}`}>
-    //                         <button onClick={() => this.props.editmessage(this.props.itemID)} className="dropdown-item btn btn-primary" >ערוך/י</button>
-    //                     </Link>
-
-    //                     <button onClick={(e) => this.props.deletemessage(e, this.props.itemID, this.props.isFav)} className="dropdown-item btn btn-primary" >מחק/י</button>
-    //                     {this.props.isFav ?
-    //                         <button onClick={(e) => this.props.changelocation(e, this.props.itemID, this.props.isFav)} className="dropdown-item btn btn-primary" >הסר/י ממועדפים</button>
-    //                         :
-    //                         <button onClick={(e) => this.props.changelocation(e, this.props.itemID, this.props.isFav)} className="dropdown-item btn btn-primary" >הוסף/י למועדפים</button>
-    //                     }
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     );
-    // }
